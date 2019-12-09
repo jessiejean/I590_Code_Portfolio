81f85769e61c2cb2084295c9b0f2f576d72065d9
jessiejean <47954284+jessiejean@users.noreply.github.com>
2019-12-09 00:49:37 +0000

    Update 1-Import.py

--- Python/1-Import.py
+++ Python/1-Import.py
@@ -1,1 +1,6 @@
+#Import csv and excel files through panda library
+import pandas as pd
+df = pd.read_csv("/tmp/tmp07wuam09/data/cereal.csv")
+df = pd.read_excel (r'Path where the Excel file is stored\File name.xlsx') 
 
+#Source: https://chrisalbon.com/python/data_wrangling/pandas_dataframe_importing_csv/
