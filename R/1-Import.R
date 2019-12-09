fc24db623bd9d079011c4a2f6559372f69a57efb
jessiejean <47954284+jessiejean@users.noreply.github.com>
2019-12-09 00:20:55 +0000

    Update 1-Import.R

--- R/1-Import.R
+++ R/1-Import.R
@@ -15,3 +15,6 @@ my_data <- read_excel("my_file.xlsx")
 my_data <- read_excel("my_file.xlsx", sheet = "data")
 # Import worksheet by its index
 my_data <- read_excel("my_file.xlsx", sheet = 2)
+
+#Source: https://www.statmethods.net/input/importingdata.html 
+#Source: https://www.datacamp.com/community/tutorials/r-data-import-tutorial
