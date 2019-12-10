b3803115e7fab1b1ecbbaf1106e2c5c697c3c1a9
jessiejean <47954284+jessiejean@users.noreply.github.com>
2019-12-10 22:57:24 +0000

    Update 3-Visualization.py

--- Python/3-Visualization.py
+++ Python/3-Visualization.py
@@ -17,6 +17,9 @@ plt.hist(np.log10(df.variable), bins = 20)
     df.set_xlabel(x_label)
     df.set_ylabel(y_label)
     
+#Create density plot: shows density plots for all numeric variables in dataset
+    df.plot(kind = 'density', subplots=True, layout = (3,3), sharex=False)
+         
 # Create ScatterPlot: set the linewidth (lw), color and transparency (alpha) of the line  
    df.scatter(df.x, df.y, s = 10, color = 'choose color', alpha = 1)
     # Label the axes and provide a title
@@ -26,3 +29,20 @@ plt.hist(np.log10(df.variable), bins = 20)
  
 # Create BarPlot
     df.bar(df.x, df.y, color = 'choose color', align = 'center')
+         
+#Create Boxplot
+     data.plot(kind = 'box', subplots = True, layout = (3,3), sharex = False, sharey = False)
+  
+#Create correlation plot: choose coolwarm as color scheme
+    df.corr()
+    corr.style.background_gradient(cmap='coolwarm')
+         
+#Create scatterplot matrix: creates scatterplots and a central histogram of all numeric variables in dataset
+    import matplotlib.pyplot as plt
+    import pandas
+    from pandas.plotting import scatter_matrix
+    
+    scatter_matrix(df)
+    plt.show()
+         
+   #Source: https://machinelearningmastery.com/visualize-machine-learning-data-python-pandas/
