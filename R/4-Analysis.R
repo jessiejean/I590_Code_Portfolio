cc3f5b343566db2a63882e564269437b4f3d259c
jessiejean <47954284+jessiejean@users.noreply.github.com>
2019-12-10 01:43:08 +0000

    Update 4-Analysis.R

--- R/4-Analysis.R
+++ R/4-Analysis.R
@@ -1,1 +1,16 @@
-1-Import
+#Create scatterplot of 2 variables and draw fitted line
+
+plot(y ~ x, data = dataset, col = "choosecolor", pch = 20,
+     main = "Title for Chart")
+fittedline = lm(y ~ x, data = dataset)
+abline(fittedline, col = "choosecolor", lwd = 3)
+
+
+#Create QQ plot to draw correlation between sample and normal distribution
+qqnorm(dataset$variable, pch = 1, frame = FALSE)
+qqline(dataset$variable, col = "choosecolor", lwd = 2)
+
+
+
+
+Source: https://rpubs.com/williamsurles/294096 
