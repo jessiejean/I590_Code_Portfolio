992fc141b70a35b535fc9a74821153d12521a680
jessiejean <47954284+jessiejean@users.noreply.github.com>
2019-12-12 03:10:31 +0000

    Update 4-Analysis.py

--- Python/4-Analysis.py
+++ Python/4-Analysis.py
@@ -1,20 +1,128 @@
-#Assignment 9
-#Question 1: Use NumPy to edit csv and save as.npy file
-#Date: 10/22/2017
-#Jessica Johnson
+#Regression analysis
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+import seaborn as seabornInstance 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+%matplotlib inline
+
+
+#regression analysis of weather conditions
+
+#create datasets of min/max temp
+X = dataset['MinTemp'].values.reshape(-1,1)
+y = dataset['MaxTemp'].values.reshape(-1,1)
+
+#Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#import linear regression algorithm
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train) #training the algorithm
+
+#To retrieve the intercept:
+print(regressor.intercept_)
+#For retrieving the slope:
+print(regressor.coef_)
+
+#make predictions
+y_pred = regressor.predict(X_test)
 
 
 
-def main():
-    import numpy as np
-    from numpy import genfromtxt
 
-    s=genfromtxt("Scores_all.csv", delimiter=",")
-    scores=s[1:]
-    scores[np.isnan(scores)] = 0
-    scores = scores.astype(int)    
-    np.save("scores",scores)
-  
-  
-      
-main()
+#Cluster analysis - kmeans
+#K-means clustering is a clustering algorithm that aims to partition n observations into k clusters.
+#step1: Initialisation – K initial “means” (centroids) are generated at random
+#step2: Assignment – K clusters are created by associating each observation with the nearest centroid
+#step3: Update – The centroid of the clusters becomes the new mean
+
+
+#import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#initializing centroids to be generated at random
+np.random.seed(200)
+k = 3
+centroids[i] = [x, y]
+centroids = {
+    i+1: [np.random.randint(0, 80), np.random.randint(0, 80)]
+    for i in range(k)}
+
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x'], df['y'], color='k')
+colmap = {1: 'r', 2: 'g', 3: 'b'}
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colmap[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+plt.show()
+
+
+#Assign observation to nearest centroid
+def assignment(df, centroids):
+    for i in centroids.keys():
+        # sqrt((x1 - x2)^2 - (y1 - y2)^2)
+        df['distance_from_{}'.format(i)] = (
+            np.sqrt(
+                (df['x'] - centroids[i][0]) ** 2
+                + (df['y'] - centroids[i][1]) ** 2
+            )
+        )
+    centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
+    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
+    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+    df['color'] = df['closest'].map(lambda x: colmap[x])
+    return df
+
+df = assignment(df, centroids)
+print(df.head())
+
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colmap[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+plt.show()
+
+
+#Update Stage to update with new means
+
+import copy
+
+old_centroids = copy.deepcopy(centroids)
+
+def update(k):
+    for i in centroids.keys():
+        centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
+        centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
+    return k
+
+centroids = update(centroids)
+    
+fig = plt.figure(figsize=(5, 5))
+ax = plt.axes()
+plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colmap[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+for i in old_centroids.keys():
+    old_x = old_centroids[i][0]
+    old_y = old_centroids[i][1]
+    dx = (centroids[i][0] - old_centroids[i][0]) * 0.75
+    dy = (centroids[i][1] - old_centroids[i][1]) * 0.75
+    ax.arrow(old_x, old_y, dx, dy, head_width=2, head_length=3, fc=colmap[i], ec=colmap[i])
+plt.show()
+
+
+
+#source:http://benalexkeen.com/k-means-clustering-in-python/
+#https://towardsdatascience.com/a-beginners-guide-to-linear-regression-in-python-with-scikit-learn-83a8f7ae2b4f
+
